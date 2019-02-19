3735a5f5897508404ddb917d22d4745cf9b3a189
“Brett <bretttewell@gmail.com>
2019-02-19 16:26:16 -0600

    classwork - OOR exercises

---
+++ employee.rb
@@ -0,0 +1,15 @@
+# # model an employee
+# # keep track of their first_name, last_name, salary, and active status
+
+# employee1 = ["Majora", "Carter", 80000, true]
+# employee2 = ["Danilo", "Campos", 70000, false]
+
+# # "Majora Carter makes $80000 per year"
+
+# p employee1[0] + " " + employee1[1].to_s + " Makes $" + employee1[2] + " per year."
+
+#hash
+
+# employee1 - {"first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true}
+# employee2 = {:first_name => "Danilo", :last_name => "Campos", :salary => "70000", :active => false} 
+
---
+++ fruit.rb
@@ -0,0 +1,6 @@
+fruit = ["apple", "banana", "mango", "kiwi"]
+
+  
+  fruit.each do |fruit|
+  p fruit
+end
