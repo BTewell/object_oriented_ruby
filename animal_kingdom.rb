d10ac5e9a8b55135af2300363cb447100dfb8b87
“Brett <bretttewell@gmail.com>
2019-02-21 18:15:12 -0600

    OOR-creating the animal class - in progress

---
+++ animal_kingdom.rb
@@ -0,0 +1,43 @@
+# class Animal
+#   attr_accessor :type, :name, :eat, :state 
+#   def initialize(input_options)
+#     @type = input_options[:type]
+#     @name = input_options[:name]
+#     @eat = input_options[:eat]
+#     @sleep = input_options[:sleep]
+#     @wake = input_options[:wake]
+#   end
+
+#   def sl 
+# end
+
+# animal1 = Animal.new(type:"cheetah", name:"Chester")
+
+# p animal1
+
+# class Animal
+#   attr_accessor :state
+#   def initialize(kind)
+#     @type = kind
+#     @state = "awake"
+#   end
+
+#   def eat (food)
+#     puts "Yum! I, as a #{@type} love to eat #{food}!"
+#   end
+
+#   def sleep
+#     @state = "asleep"
+#   end
+
+#   def wake
+#     @state = "awake"
+#   end
+# end
+
+# bird = Animal.new("Parrot")
+# bird.eat("Crackers")
+# bird.sleep
+
+# puts bird.state
+
